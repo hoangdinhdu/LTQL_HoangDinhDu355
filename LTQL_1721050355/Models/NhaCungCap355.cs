2f001dc3f5681ee813b5d486a8f2ed09beca7e42
HOANG DINH DU <duhoang1902@gmail.com>
2021-12-21 13:07:49 +0700

    Create_Table_NCC_HDD355

---
+++ LTQL_1721050355/Models/NhaCungCap355.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace LTQL_1721050355.Models
+{
+    [Table("NhaCungCap355")]
+
+    public class NhaCungCap355
+    {
+        [Key]
+        [Display(Name = "ID Nhà Cung Cấp")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Manhacungcap { get; set; }
+        [Display(Name = "Tên Nhà Cung Cấp")]
+
+        [StringLength(50)]
+
+        public string Tennhacungcap { get; set; }
+       
+    }
+}
